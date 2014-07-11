4e444b47f3e1a210545e4b715de5a29d5be430c0
Jethro Muller <jethro@yola.com>
2014-07-11 17:12:17 +0200

    Remove code only for testing.

--- doorservice/open_door.py
+++ doorservice/open_door.py
@@ -1,20 +1,16 @@
 import time
+import RPi.GPIO as GPIO
 
 def open_door():
-    try:
-        import RPi.GPIO as GPIO
+    GPIO.setmode(GPIO.BCM)
 
-        GPIO.setmode(GPIO.BCM)
+    pin = 0
 
-        pin = 0
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
 
-        GPIO.setup(pin, GPIO.OUT)
-        GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(pin, GPIO.LOW)
+    time.sleep(0.5)
+    GPIO.output(pin, GPIO.HIGH)
 
-        GPIO.output(pin, GPIO.LOW)
-        time.sleep(0.5)
-        GPIO.output(pin, GPIO.HIGH)
-
-        return True
-    except RuntimeError:
-        return False
+    return True
