6838348907e50c1b891dc6bb031363c4f371ae79
Jethro Muller <jethro@yola.com>
2014-07-14 15:48:24 +0200

    Add extra button and associated processing code.

--- doorservice/open_door.py
+++ doorservice/open_door.py
@@ -1,11 +1,28 @@
 import time
 
-def open_door():
+def open_outer_door():
     import RPi.GPIO as GPIO
     
     GPIO.setmode(GPIO.BCM)
 
     pin = 0
+
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+
+    GPIO.output(pin, GPIO.LOW)
+    time.sleep(0.5)
+    GPIO.output(pin, GPIO.HIGH)
+
+    return True
+
+
+def open_inner_door():
+    import RPi.GPIO as GPIO
+
+    GPIO.setmode(GPIO.BCM)
+
+    pin = 1
 
     GPIO.setup(pin, GPIO.OUT)
     GPIO.output(pin, GPIO.HIGH)
--- doorservice/views.py
+++ doorservice/views.py
@@ -2,16 +2,19 @@ from flask import render_template, request
 from doorservice import app
 from auth import auth_decorator
 
-from open_door import open_door
+from open_door import open_inner_door, open_outer_door
 
 
 @app.route("/", methods=['GET'])
 @auth_decorator()
 def index():
-    context = {
-        'unset': True,
-        'auth': True,
-    }
+    context = dict()
+    outer_door = dict()
+    inner_door = dict()
+    context['auth'] = True
+
+    context['outer_door'] = outer_door
+    context['inner_door'] = inner_door
     return render_template('index.html', **context)
 
 
@@ -19,21 +22,30 @@ def index():
 @auth_decorator()
 def opendoor():
     context = dict()
-
-    context['success'] = False
+    outer_door = dict()
+    inner_door = dict()
     context['auth'] = True
 
-    if request.form.get('door-button') == 'open-door':
-        context['success'] = open_door()
+    if request.form.get('outer-door-button') == 'open-door':
+        outer_door['set'] = True
+        outer_door['success'] = open_outer_door()
+    elif request.form.get('inner-door-button') == 'open-door':
+        inner_door['set'] = True
+        inner_door['success'] = open_inner_door()
 
+    context['outer_door'] = outer_door
+    context['inner_door'] = inner_door
     return render_template('index.html', **context)
 
 
 @app.errorhandler(404)
 @auth_decorator()
 def not_found(error):
-    context = {
-        'auth': True,
-        'success': False,
-    }
+    context = dict()
+    outer_door = dict()
+    inner_door = dict()
+    context['auth'] = True
+
+    context['outer_door'] = outer_door
+    context['inner_door'] = inner_door
     return render_template('index.html', **context)
