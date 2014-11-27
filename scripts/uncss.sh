c4c1494b93cdec11072af2665f6901b425ecda99
William Stewart <william@synthasite.com>
2014-11-27 12:32:29 +0200

    Replace npm/bower with uncss'ed CSS and its script
    So the script could possibly do with some comments.
    
    I touch the uncss'ed file, so that it exists when uncss parses the
    template, because if uncss can't find it, it breaks. I tried using the
    `--ignoreSheets` command to ignore that stylesheet, but it doesn't do
    what I want.
    
    I copied `bootstrap.css` to a simpler place, so the paths were less
    messy. I left bower installing to default bower_components so we have
    less stuff in the repo.

---
+++ scripts/uncss.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -exu
+cd "$(dirname "$0")/.."
+
+rm -rf node_modules bower_components
+rm doorservice/static/css/doorservice.uncss.css
+
+npm install bower uncss
+$(npm bin)/bower install bootstrap#3.x.x
+
+touch doorservice/static/css/doorservice.uncss.css
+cp bower_components/bootstrap/dist/css/bootstrap.css doorservice/static/css/
+$(npm bin)/uncss -H doorservice -i "/.btn-(primary|success|danger)/" -s ../static/css/bootstrap.css, ../static/css/doorservice.css doorservice/templates/index.html > doorservice/static/css/doorservice.uncss.css
