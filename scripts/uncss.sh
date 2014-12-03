482ff4c33d18a594d3794a0b5781ca73a75795eb
William Stewart <william@synthasite.com>
2014-12-03 09:29:50 +0200

    Put quotes around Boostrap version, because #

--- scripts/uncss.sh
+++ scripts/uncss.sh
@@ -7,7 +7,7 @@ rm -rf node_modules bower_components
 rm doorservice/static/css/doorservice.uncss.css
 
 npm install bower uncss
-$(npm bin)/bower install bootstrap#3.x.x
+$(npm bin)/bower install "bootstrap#3.x.x"
 
 touch doorservice/static/css/doorservice.uncss.css
 cp bower_components/bootstrap/dist/css/bootstrap.css doorservice/static/css/
